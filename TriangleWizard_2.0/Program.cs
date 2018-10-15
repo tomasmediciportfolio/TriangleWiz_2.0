4d6c23505ddc058348ed5a2976b83ba91e401810
tomasmedici <tomas@medici.dk>
2018-10-15 20:04:05 +0200

    Working solution

---
+++ TriangleWizard_2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TriangleWizard_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleWiz wiz = new TriangleWiz();
+
+            List<List<int>> triangleList = new List<List<int>>();
+
+            triangleList.Add(new List<int> { 215 });
+            triangleList.Add(new List<int> { 192, 124 });
+            triangleList.Add(new List<int> { 117, 269, 442 });
+            triangleList.Add(new List<int> { 218, 836, 347, 235 });
+            triangleList.Add(new List<int> { 320, 805, 522, 417, 345 });
+            triangleList.Add(new List<int> { 229, 601, 728, 835, 133, 124 });
+            triangleList.Add(new List<int> { 248, 202, 277, 433, 207, 263, 257 });
+            triangleList.Add(new List<int> { 359, 464, 504, 528, 516, 716, 871, 182 });
+            triangleList.Add(new List<int> { 461, 441, 426, 656, 863, 560, 380, 171, 923 });
+            triangleList.Add(new List<int> { 381, 348, 573, 533, 448, 632, 387, 176, 975, 449 });
+            triangleList.Add(new List<int> { 223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444 });
+            triangleList.Add(new List<int> { 330, 131, 333, 928, 376, 733, 017, 778, 839, 168, 197, 197 });
+            triangleList.Add(new List<int> { 131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928 });
+            triangleList.Add(new List<int> { 223, 626, 034, 683, 839, 052, 627, 310, 713, 999, 629, 817, 410, 121 });
+            triangleList.Add(new List<int> { 924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233 });
+
+            (int, List<int>) result = wiz.CalculatePath(triangleList);
+
+            Console.WriteLine("Output: ");
+            Console.WriteLine("Max sum: {0} ", result.Item1);
+            Console.WriteLine("Path: " + String.Join(", ", result.Item2));
+        }
+    }
+}
---
+++ TriangleWizard_2.0/TriangleWiz.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriangleWizard_2
+{
+    public class TriangleWiz
+    {
+        public TriangleWiz()
+        {
+            allpaths = new List<List<int>>();
+            container1 = new List<List<int>>();
+            container2 = new List<List<int>>();
+        }
+
+        public (int, List<int>) CalculatePath(List<List<int>> bruttolist)
+        {
+            int size = bruttolist.Count;
+            if (size % 2 != 0) //if height odd, use container1
+            {
+                AllPaths(bruttolist);
+                AllLists(bruttolist);
+                ValidLists(bruttolist);
+                (int count, int sum) = HighestSum(bruttolist);
+                return (sum, container1[count]);
+            }
+            else //else use container2
+            {
+                AllPaths(bruttolist);
+                AllLists(bruttolist);
+                ValidLists(bruttolist);
+                (int count, int sum) = HighestSum(bruttolist);
+                return (sum, container2[count]);
+            }
+        }
+
+        public (int, int) HighestSum(List<List<int>> bruttolist)
+        {
+            int size = bruttolist.Count;
+            if (size % 2 != 0) //if height odd, use container1
+            {
+                List<int> sumlist = new List<int>();
+                foreach (List<int> sublist in container1)
+                {
+                    int sumAf = sublist.Sum();
+                    sumlist.Add(sumAf);
+                }
+                int maxsum = sumlist.Max();
+                int c = 0;
+                for (int i = 0; sumlist[i] != maxsum; i++)
+                {
+                    c++;
+                }
+                return (c, maxsum);
+            }
+            else //if height even, use container2
+            {
+                List<int> sumlist = new List<int>();
+                foreach (List<int> sublist in container2)
+                {
+                    int sumAf = sublist.Sum();
+                    sumlist.Add(sumAf);
+                }
+                int maxsum = sumlist.Max();
+                int c = 0;
+                for (int i = 0; sumlist[i] != maxsum; i++)
+                {
+                    c++;
+                }
+                return (c, maxsum);
+            }
+        }
+
+        public void ValidLists(List<List<int>> bruttolist)
+        {
+            int size = bruttolist.Count;
+            if (bruttolist[0][0] % 2 != 0) //The top row is odd
+            {
+                if (size % 2 != 0) //If the height of the triangle is odd, we take data from container2 
+                {
+                    container1.Clear();
+                    foreach (List<int> path in container2) //We run through all possible paths
+                    {
+                        int count = 0; //Count validates if a path alternates correctly, se below
+                        for (int i = 0; i < path.Count; i++) //We run through all entries of a path, to check if they alternate correctly
+                        {
+                            if (i % 2 != 1) //If we have a number in the path corresponding to an even row
+                            {
+                                if (path[i] % 2 != 0) //We know it has to be odd
+                                {
+                                    count++;  //Therefor we add to count
+                                }
+                            }
+                            else  //If we have a number in the path corresponding to an odd row
+                            {
+                                if (path[i] % 2 != 1) //We know it has to be even
+                                {
+                                    count++; //Therefor we add to count
+                                }
+                            }
+                        }
+                        if (count == size) //Only if all parameters are met
+                        {
+                            container1.Add(path);  //The path is added to the selected list
+                        }
+                    }
+                }
+                else //We repeat the above in case the height of the triangle is even, and we have to take data from container1
+                {
+                    container2.Clear();
+                    foreach (List<int> path in container1) //We run through all possible paths
+                    {
+                        int count = 0; //Count validates if a path alternates correctly, se below
+                        for (int i = 0; i < path.Count; i++)
+                        {
+                            if (i % 2 != 1) //If we have a number in the path corresponding to an even row
+                            {
+                                if (path[i] % 2 != 0) //We know it has to be odd
+                                {
+                                    count++;  //Therefor we add to count
+                                }
+                            }
+                            else  //If we have a number in the path corresponding to an odd row
+                            {
+                                if (path[i] % 2 != 1) //We know it has to be even
+                                {
+                                    count++; //Therefor we add to count
+                                }
+                            }
+                        }
+                        if (count == size) //Only if all parameters are met
+                        {
+                            container2.Add(path);  //The path is added to the selected list
+                        }
+                    }
+                }
+            }
+            else
+            {
+                
+            }
+
+        }
+
+        public void AllLists(List<List<int>> bruttolist)
+        {
+            int size = bruttolist.Count;
+            if (size % 2 != 0)
+            {
+                container2.Clear();
+                foreach (List<int> elem in container1)
+                {
+                    List<int> templiste2 = new List<int>();
+                    for (int i = 0; i < size; i++)
+                    {
+                        templiste2.Add(bruttolist[i][elem[i]]);
+                    }
+                    container2.Add(templiste2);
+                }
+            }
+            else
+            {
+                container1.Clear();
+                foreach (List<int> elem in container2)
+                {
+                    List<int> templiste2 = new List<int>();
+                    for (int i = 0; i < size; i++)
+                    {
+                        templiste2.Add(bruttolist[i][elem[i]]);
+                    }
+                    container1.Add(templiste2);
+                }
+            }
+        }
+
+        public void AllPaths(List<List<int>> bruttolist)
+        {
+            container1.Add(new List<int> { 0 });
+            int size    = bruttolist.Count;
+
+            for (int i = 1; i < size; i++)
+            {
+                if ( i % 2 != 0 )
+                {
+                    container2.Clear();
+                    foreach (List<int> liste in container1)
+                    {
+                        List<int> templiste0 = new List<int>(liste);
+                        templiste0.Add(liste.Last());
+                        container2.Add(templiste0);
+
+                        List<int> templiste1 = new List<int>(liste);
+                        templiste1.Add(liste.Last() + 1);
+                        container2.Add(templiste1);
+                    }
+                }
+                else 
+                {
+                    container1.Clear();
+                    foreach (List<int> liste in container2)
+                    {
+                        List<int> templiste0 = new List<int>(liste);
+                        templiste0.Add(liste.Last());
+                        container1.Add(templiste0);
+
+                        List<int> templiste1 = new List<int>(liste);
+                        templiste1.Add(liste.Last() + 1);
+                        container1.Add(templiste1);
+                    }
+                }
+            }
+        }
+
+        public List<List<int>> allpaths;
+        public List<List<int>> container1;
+        public List<List<int>> container2;
+    }
+}
